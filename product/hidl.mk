b5df954433b577033f17b387c976e6ccb96d780b
Louis Popi <theh2o64@gmail.com>
2017-08-25 08:25:29 +0200

    msm8916-common: hidl: Don't use 8909 vendor headers
    * There must be another way

--- product/hidl.mk
+++ product/hidl.mk
@@ -1,6 +1,3 @@
 # HIDL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
-
-# MSM8916 Headers
-PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8x09/kernel-headers
